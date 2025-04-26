9bca703f93a29603046e32775cf5c9f0da0fc791
ypatodkar <ypatodkar@scu.edu>
2025-04-26 16:37:07 -0700

    Landing page

--- dead-last/src/ui/components/App.jsx
+++ dead-last/src/ui/components/App.jsx
@@ -5,25 +5,19 @@ import "@spectrum-web-components/theme/express/theme-light.js";
 
 // To learn more about using "swc-react" visit:
 // https://opensource.adobe.com/spectrum-web-components/using-swc-react/
-import { Button } from "@swc-react/button";
 import { Theme } from "@swc-react/theme";
 import React from "react";
 import "./App.css";
+import Storytelling from './Storytelling';
+
 
 const App = ({ addOnUISdk, sandboxProxy }) => {
-    function handleClick() {
-        sandboxProxy.createRectangle();
-    }
 
     return (
         // Please note that the below "<Theme>" component does not react to theme changes in Express.
         // You may use "addOnUISdk.app.ui.theme" to get the current theme and react accordingly.
         <Theme system="express" scale="medium" color="light">
-            <div className="container">
-                <Button size="m" onClick={handleClick}>
-                    Create Rectangle
-                </Button>
-            </div>
+                <Storytelling />
         </Theme>
     );
 };
